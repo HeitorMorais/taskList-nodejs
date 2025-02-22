7803807ac7f253b6f62623ab5c93c0e2733c4390
HeitorMorais <heitormorais2003@gmail.com>
2025-02-22 10:42:40 -0300

    files in the wrong folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
-const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express()
-app.use(express.json())
+app.use('/', routes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
